d0300754bfa6be221ff363fdb0ab1d6583c7bab7
Daiki <daiki.miura@prum.jp>
2023-08-10 00:29:14 +0900

    add: styles

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { ImCheckboxChecked, ImCheckboxUnchecked } from 'react-icons/im';
+import styled from 'styled-components';
+import { AiFillEdit } from 'react-icons/ai';
 
 interface Todo {
   id: number;
@@ -15,6 +17,63 @@ interface Todo {
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [searchName, setSearchName] = useState('');
+
+  const SearchAndButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  `
+
+  const SearchForm = styled.input`
+    font-size: 20px;
+    width: 100%;
+    height: 40px;
+    margin: 10px 0;
+    padding: 10px;
+  `
+
+  const RemoveAllButton = styled.button`
+    width: 16%;
+    height: 40px;
+    background: #f54242;
+    border: none;
+    font-weight: 500;
+    margin-left: 10px;
+    padding: 5px 10px;
+    border-radius: 3px;
+    color: #fff;
+    cursor: pointer;
+  `
+
+  const Row = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 7px auto;
+    padding: 10px;
+    font-size: 25px;
+  `
+
+  const CheckedBox = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0 7px;
+    color: green;
+    cursor: pointer;
+  `
+
+  const UncheckedBox = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0 7px;
+    cursor: pointer;
+  `
+
+  const EditButton = styled.span`
+    display: flex;
+    align-items: center;
+    margin: 0 7px;
+    `
 
   useEffect(() => {
     axios.get('http://localhost:3001/api/v1/todos')
@@ -59,44 +118,49 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div className="">
+      <div className="body">
+        <h1>Todo List</h1>
         <div>
           <input
+            className="search-form"
             type="text"
             placeholder="Search todo..."
             onChange={(event) => {
               setSearchName(event.target.value);
             }}
           />
-          <button onClick={removeAllTodos}>
+          <RemoveAllButton onClick={removeAllTodos}>
             Remove All
-          </button>
+          </RemoveAllButton>
         </div>
-      </div>
-      <div>
-        {todos.filter((val) => {
-          if (searchName === "") {
-            return val;
-          } else if (val.name.toLowerCase().includes(searchName.toLowerCase())) {
-            return val;
-          }
-        }).map((val, key) => (
-          <div key={key}>
-            {val.is_completed ? (
+        <div>
+          {todos.filter((val) => {
+            if (searchName === "") {
+              return val;
+            } else if (val.name.toLowerCase().includes(searchName.toLowerCase())) {
+              return val;
+            }
+          }).map((val, key) => (
+            <Row key={key}>
+              {val.is_completed ? (
+                <CheckedBox>
+                  <ImCheckboxChecked onClick={() => updateIsCompleted(key, val)} />
+                </CheckedBox>
+              ) : (
+                <UncheckedBox>
+                  <ImCheckboxUnchecked onClick={() => updateIsCompleted(key, val)} />
+                </UncheckedBox>
+              )}
               <div>
-                <ImCheckboxChecked onClick={() => updateIsCompleted(key, val)} />
+                {val.name}
               </div>
-            ) : (
-              <div>
-                <ImCheckboxUnchecked onClick={() => updateIsCompleted(key, val)} />
-              </div>
-            )}
-            <div>
-              {val.name}
-            </div>
-            {/* リンクと編集 */}
-          </div>
-        ))}
+              {/* 編集リンク追加する */}
+              <EditButton>
+                <AiFillEdit />
+              </EditButton>
+            </Row>
+          ))}
+        </div>
       </div>
     </div>
   );
